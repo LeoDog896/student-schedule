f405d7cff82390375194f21216ddbc4bab96477a
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-09-20 12:38:26 +0000

    Updated teacher button on navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,7 +5,7 @@
 	<a class="siteName" href="/"><h1>Site Name</h1></a>
 	<div class="buttons">
 		<a href="/admin" class="button">Admin</a>
-		<a href="/teacher" class="button">Teacher</a>
+		<a href="/teacher/attendance" class="button">Teacher</a>
 		<a href="/" class="button">Schedule</a>
 		<a href="/attendance" class="button">Attendance</a>
 		<a href="/help" class="button">Help</a>
