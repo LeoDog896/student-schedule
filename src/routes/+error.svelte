0034da92e6d3c858c652a279d095f3b60d316f70
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-09-20 07:15:59 -0400

    feat: UI (#1)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<div class="wrap">
+	<div class="subWrap">
+		<h1>404</h1>
+		<hr>
+		<p>Page not found</p>
+	</div>
+</div>
+
+<style>
+	.wrap {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+		color: var(--textDark);
+	}
+	.subWrap {
+		display: flex;
+		flex-direction: row;
+	}
+	.subWrap h1 {
+		font-size: 4rem;
+		margin: 0px 20px;
+	}
+	.subWrap hr {
+		border-left: 1px solid var(--textDark);
+	}
+	.subWrap p {
+		font-size: 1.5rem;
+		margin: 0px 10px;
+		display: flex;
+		align-items: center;
+	}
+</style>
